fad8250fbe054683970c6d69e8911b52e33a9ac8
Abril Gu8zman Barrera <230350@utxicotepec.edu.mx>
2025-01-13 15:13:02 -0600

    Practica3
    Agrgacion del formato

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 //Configuración de la sesión
 app.use(session({
-    secret: 'yinyerina', //Secreto para firmar la cookie de sesión
+    secret: 'mi-clave-secreta', //Secreto para firmar la cookie de sesión
     resave:false,  //No resguardar la sesión si no ha sido modificada
     saveUninitialized:false,  //Guardar la sesión aunque no haya sido inicializada
     cookie:{secure:false}  //Usar secure:true solo si usas HTTPS
@@ -25,37 +25,43 @@ app.use((req,res, next)=>{
 });
 
 
+app.get('/login/:User',(req,res)=>{
+    req.session.User=req.params.User;
+    res.send("Usuario guardado");
+})
+
 //Ruta para mostrar la información de la sesión
 app.get('/session',(req,res)=>{
     if(req.session){
+        const User = req.session.User;
         const sessionId = req.session.id;
         const createdAt = req.session.createdAt;
         const lastAccess = req.session.lastAccess;
-        const sessionDuration = (new Date() - createdAt)/1000; //Duración de la sesión en segundos
+        const sessionDuration = (new Date() - new Date(createdAt))/1000; //Duración de la sesión en segundos
+        console.log(`La duración de la sesión es de ${sessionDuration} segundos.`);
         
         res.send(`
             <h1>Detalles de la sesion</h1>
+            <p><strong>Usuario:</strong>${User}</p>
             <p><strong>ID de sesión:</strong>${sessionId}</p>
             <p><strong>Fecha de creación de la sesión:</strong>${createdAt}</p>
             <p><strong>último acceso:</strong>${lastAccess}</p>
             <p><strong>Duración de la sesión (en segundos):</strong>${sessionDuration}</p>
             `);
-    }})
-
-//Ruta para cerra la secion 
-app.get('/logout',(res,req) =>{
-    req.session.destroy((err)=>{
-        if(err){
-            return res.send('Error al iniciar sesion ');
-        }
-        res.send('<h1>Secion cerrada exitosamente</h1>')
-    })
+    }
 })
 
-//Iniciar el servidor en el puerto 
-app.listen(3000,()=> {
-    console.log('Servidor en puerto 3000');
+//Ruta para cerrar la sesión
+app.get('/logout',(req,res)=>{
+    req.session.destroy((err)=>{
+        if(err){
+            return res.send('Error al cerrar sesion.');
+        }
+        res.send('<h1>Sesión cerrada exitosamente.</h1>');
+    });
 });
 
-
-
+//Iniciar el servidor en el puerto 3000
+app.listen(3000,()=>{
+    console.log('Servidor corriendo en el puerto 3000');
+});
